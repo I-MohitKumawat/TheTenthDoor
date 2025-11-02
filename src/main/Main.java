15e3f2b5a9a0ba9811d152eeb87b8c4f92ac82fa
Mohit <i.kumawat.mohit@gmail.com>
2025-11-02 09:24:59 +0530

    feat: Main and GamePanel

---
+++ src/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage){
+        GamePanel gamePanel = new GamePanel();
+        Scene scene = new Scene(gamePanel);
+        stage.setTitle("The Tenth Door");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
